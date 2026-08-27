c7694cf112e48ce82728b32ded17b8aa4bd40f6d
TOUMAMUCHISUKI <amvindexchan@gmail.com>
2026-08-27 17:05:26 -0500

    feat: add profile retrieval method

--- Assets/Services/Player/PlayerProfileService.cs
+++ Assets/Services/Player/PlayerProfileService.cs
@@ -1,3 +1,13 @@
 public class PlayerProfileService
 {
+    private PlayerProfile profile = new PlayerProfile
+    {
+        playerId = "player-001",
+        playerName = "Player"
+    };
+
+    public PlayerProfile GetProfile()
+    {
+        return profile;
+    }
 }
